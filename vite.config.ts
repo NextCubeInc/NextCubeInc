c80183e21f11157a9689ee31b0a77f58c1130ddc
gubazzs <gr.marques@live.com>
2025-09-09 20:49:22 +0000

    deply configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+const repoName = "/NextCubeInc"
 export default defineConfig(({ mode }) => ({
+  base: repoName, 
   server: {
     host: "::",
     port: 8080,
